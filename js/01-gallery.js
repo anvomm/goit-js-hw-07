aa1d7a18630fe2efcc4054182cbad61ac4b5e335
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-13 00:08:31 +0300

    markup created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGallery() {
+    const markup = galleryItems.map(item => {
+    const img = `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></div>`;
+    return img;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+createGallery();
+
+
