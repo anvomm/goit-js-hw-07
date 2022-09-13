7b79e3ec5a92c95ce00d3e8e6a6831ee4504a6a7
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-13 23:04:33 +0300

    task finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,8 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
+gallery.addEventListener('click', onClickEvent);
+
 function createGallery() {
     const markup = galleryItems.map(item => {
     const img = `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></div>`;
@@ -12,39 +14,23 @@ function createGallery() {
 gallery.insertAdjacentHTML('beforeend', markup);
  }; 
 
-
-/* instance.show() */
 createGallery(); 
-
-
-/* const images = document.querySelectorAll('.gallery__image');
-images.forEach(img => {
-    const instance = basicLightbox.create(
-	img
-)
-}) */
-gallery.addEventListener('click', onClickEvent);
 
 function onClickEvent(event) {
     event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
+    /* if (event.target.nodeName !== 'IMG') {
         return;
-    };
-    basicLightbox.create(`<img src=${event.target.dataset.source}>`).show();
+    }; */
+    const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`);
+    instance.show();
+    window.addEventListener('keydown', onEscPressEvent);
 
-    /* event.target);
-    event.target.src = event.target.dataset.source; */
+    function onEscPressEvent(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+    
+}
 }
 
-/* const instance = basicLightbox.create(
-    markup,
-    {
-	visible: true
-}
-);
-gallery.insertAdjacentHTML('beforeend', instance); */
 
-/* const instance = basicLightbox.create(
-	gallery
-) */
-/* console.log(basicLightbox.visible()) */
