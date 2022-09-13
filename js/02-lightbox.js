b7fdc4cf3e734590168146de5f22de52e45a4e47
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-13 23:58:05 +0300

    task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+/* gallery.addEventListener('click', onClickEvent); */
+
+function createGallery() {
+    const markup = galleryItems.map(item => {
+    const img = `<a class="gallery__item" href=${item.original}><img class="gallery__image" src=${item.preview} alt=${item.description} /></a>`;
+    return img;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+ }; 
+
+createGallery(); 
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
